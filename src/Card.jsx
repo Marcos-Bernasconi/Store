215f402b0f097d6e0ccd11aa744f03e300dfb7ba
Marcos Bernasconi <xb3rna@gmail.com>
2023-02-25 23:38:59 +0100

    Finished carousel

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,25 @@
-export function Card({ name, description, image }) {
-  return (
-    <>
-      <div className="relative w-full flex-none  rounded-2xl shadow-xl bg-secondary mx-auto h-auto overflow-hidden ">
-        <div className="p-8 absolute z-10">
-          <p className="uppercase text-sm">{name}</p>
-          <p className="font-semibold text-2xl mt-4">Big and Bigger</p>
-          <p className="mt-2">{description}</p>
-        </div>
-
-        <img className=" absolute bottom-0 z-0" src={image} alt="Apple" />
+export function Card({ name, title, description, image, light }) {
+  return light ? (
+    <div className="snap-center relative w-full flex-none  rounded-2xl shadow-xl bg-secondary mx-auto h-auto overflow-hidden ">
+      <div className="p-8 absolute z-10">
+        <p className="uppercase text-sm text-neutral-500">{name}</p>
+        <p className="font-semibold text-2xl mt-4">{title}</p>
+        <p className="mt-2 whitespace-pre-wrap">{description}</p>
       </div>
-    </>
+
+      <img className=" absolute bottom-0 z-0" src={image} alt="Apple" />
+    </div>
+  ) : (
+    <div className="snap-center relative w-full flex-none  rounded-2xl shadow-xl bg-black mx-auto h-auto overflow-hidden ">
+      <div className="p-8 absolute z-10">
+        <p className="uppercase text-sm text-base-100">{name}</p>
+        <p className="font-semibold text-2xl mt-4 text-secondary ">{title}</p>
+        <p className="mt-2 text-secondary whitespace-pre-wrap ">
+          {description}
+        </p>
+      </div>
+
+      <img className=" absolute bottom-0 z-0" src={image} alt="Apple" />
+    </div>
   );
 }
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,33 +3,49 @@ import { Card } from "./Card";
 export function ProductList() {
   return (
     <>
-      <div className="flex w-full space-x-4 px-8 h-[30rem]  overflow-auto">
+      <div className="snap-proximity snap-x flex w-full space-x-4 px-8 h-[30rem]  overflow-auto">
         <Card
-          name="iPhone 14"
-          description="From $799 or $33.29/mo.per month for 24 mo.months before
-            trade‑inFootnote*"
-          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-iphone-14-202209_GEO_US_FMT_WHH?wid=618&hei=900&fmt=jpeg&qlt=90&.v=1661890726803"
+          name="Iphone 14 pro"
+          title="Pro. Beyond."
+          description={"From $999 or $41.62/mo.\nfor 24 mo*."}
+          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-iphone-14-pro-202209_FMT_WHH?wid=618&hei=900&fmt=p-jpg&qlt=95&.v=1661181418349"
         />
 
         <Card
           name="iPhone 14"
-          description="From $799 or $33.29/mo.per month for 24 mo.months before
-            trade‑inFootnote*"
+          title="Big and Bigger"
+          description={"From $799 or $33.29/mo.\nbefore trade‑in*"}
           image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-iphone-14-202209_GEO_US_FMT_WHH?wid=618&hei=900&fmt=jpeg&qlt=90&.v=1661890726803"
+          light="true"
         />
 
         <Card
-          name="iPhone 14"
-          description="From $799 or $33.29/mo.per month for 24 mo.months before
-            trade‑inFootnote*"
-          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-iphone-14-202209_GEO_US_FMT_WHH?wid=618&hei=900&fmt=jpeg&qlt=90&.v=1661890726803"
+          name="APPLE WATCH ULTRA"
+          title="Adventure awaits. "
+          description={"From $799 or $33.29/mo.\nfor 24 mo.*"}
+          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-watch-ultra-202209_FMT_WHH?wid=658&hei=900&fmt=p-jpg&qlt=95&.v=1660686053423"
+          light="true"
         />
 
         <Card
-          name="iPhone 14"
-          description="From $799 or $33.29/mo.per month for 24 mo.months before
-            trade‑inFootnote*"
-          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-iphone-14-202209_GEO_US_FMT_WHH?wid=618&hei=900&fmt=jpeg&qlt=90&.v=1661890726803"
+          name="Ipad Pro"
+          title="Supercharged by M2."
+          description={"From $799 or $66.58/mo.\nfor 12 mo.*"}
+          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-ipad-pro-202210_FMT_WHH?wid=618&hei=900&fmt=p-jpg&qlt=95&.v=1667423474151"
+        />
+
+        <Card
+          name="APPLE WATCH SERIES 8"
+          title="A healthy leap ahead. "
+          description={"From $399 or $16.62/mo.\nfor 24 mo*."}
+          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-watch-s8-202209_FMT_WHH?wid=658&hei=900&fmt=p-jpg&qlt=95&.v=1661792333321"
+        />
+        <Card
+          name="ipad"
+          title="Lovable. Drawable. Magical. "
+          description={"From $449 or $37.29/mo.\nfor 24 mo*."}
+          image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-40-ipad-202210_FMT_WHH?wid=618&hei=900&fmt=p-jpg&qlt=95&.v=1664628458667"
+          light="true"
         />
       </div>
     </>
